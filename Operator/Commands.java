45ae2dffb3c611be0657d9f85e2d0bf067450a47
Jeshua A. G. Guillén <uncorreoqualquiera@gmail.com>
2023-05-01 06:56:20 -0600

    Refactored "Commands"
    Changed the name of the class "Operators" for "Commands" and created the methods of the commands.

---
+++ Operator/Commands.java
@@ -0,0 +1,36 @@
+package Operator;
+
+/**
+ *
+ * @author CommonName(¿)
+ */
+public class Commands {
+    
+    public static void Plus(){
+
+    }
+
+    public static void Minus(){
+
+    }
+
+    public static void Times(){
+
+    }
+
+    public static void Obleous(){
+
+    }
+
+    public static void SquareRoot(){
+
+    }
+
+    public static void Equal(){
+
+    }
+
+    public static void PreviousAnswer(){
+        
+    }
+}
